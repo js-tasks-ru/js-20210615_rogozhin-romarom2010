893d727999eefd1af508575f53a118347947f8d6
RogozhinRA <14351711>
2021-06-20 21:49:30 +0300

    second task

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,23 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  const newArr = [...arr];
 
+  const collator = new Intl.Collator(["ru-RU"], { caseFirst: 'upper', localeMatcher: 'best fit'});
+  let enLangArr = newArr.filter(el => el.codePointAt(0) <= 220 && el.codePointAt(0) >= 65);
+  let ruLangArr = newArr.filter(el => el.codePointAt(0) > 220 || el.codePointAt(0) < 65);
+  const result = [];
+  if (param === 'asc') {
+    ruLangArr.sort((a, b) => {return collator.compare(a, b);}).forEach(el => result.push(el));
+    enLangArr.sort((a, b) => {return collator.compare(a, b);}).forEach(el => result.push(el));
+  }
+  else {
+    enLangArr.sort((a, b) => {return collator.compare(b, a);}).forEach(el => result.push(el));
+    ruLangArr.sort((a, b) => {return collator.compare(b, a);}).forEach(el => result.push(el));
+  }
+  return result;
+  // return newArr.sort((a, b) => {
+  //   return param === 'asc' ? collator.compare(a, b) : collator.compare(b, a);
+  // });
+  //b.localeCompare(a, ['ru', 'en'], { caseFirst: 'upper'}); sensitivity: 'case',
 }
--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,13 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
-
+  const bufObj = {...obj};
+  let bufArr = Object.entries(bufObj);
+  const fieldsArr = [...fields];
+  bufArr = bufArr.map(el => {
+    let ind = fieldsArr.findIndex(el1 => el1 === el[0]);
+    if (ind !== -1) {return el;}
+  })
+    .filter(el => el !== undefined);
+  return Object.fromEntries(bufArr);
 };
